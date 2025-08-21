549ec8cdb63fcbbe40e2bb1aa883d7fb5ec4f999
DariusGiannoli <152925066+DariusGiannoli@users.noreply.github.com>
2025-08-21 02:33:46 +0200

    today

--- Experiment/Pilot_V1/protocols/trainings/location_training.py
+++ Experiment/Pilot_V1/protocols/trainings/location_training.py
@@ -1,12 +1,17 @@
+
+import time
+import sys  
+import os  
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, root_dir)
 from categories.location import create_all_commands
 from core.serial_api import SerialAPI
 from shared import (DUTY, FREQ, DURATION, PULSE_DURATION, PAUSE_DURATION, NUM_PULSES)
 
-import time
 
 sleep = 5
 
-api = SerialAPI()
 
 commands = create_all_commands(DUTY, FREQ, DURATION, PULSE_DURATION, PAUSE_DURATION, NUM_PULSES)
 # Location : extract 4 lists 
@@ -16,15 +21,24 @@ pulse_horizontal = commands['pulse_horizontal']    # 12 commands
 pulse_vertical = commands['pulse_vertical']        # 12 commands
 
 if __name__ == "__main__":
-    # Send specific patterns by index
-    api.send_timed_batch(static_horizontal[0])    # First horizontal static (devices 0-1)
-    time.sleep(sleep)
     
-    api.send_timed_batch(pulse_horizontal[0])     # First horizontal pulse (devices 0-1)
-    time.sleep(sleep)
+    api = SerialAPI()
+    ports = api.get_serial_ports()
+    if ports and api.connect(ports[2]):
+        
+        # Send specific patterns by index
+        api.send_timed_batch(static_horizontal[0])    # First horizontal static (devices 0-1)
+        time.sleep(sleep)
+        
+        api.send_timed_batch(pulse_horizontal[0])     # First horizontal pulse (devices 0-1)
+        time.sleep(sleep)
 
-    api.send_timed_batch(pulse_vertical[0])       # First vertical pulse (devices 0-4)
-    time.sleep(sleep)
+        api.send_timed_batch(pulse_vertical[0])       # First vertical pulse (devices 0-4)
+        time.sleep(sleep)
 
-    api.send_timed_batch(static_vertical[0])      # First vertical static (devices 0-4)
-    time.sleep(sleep)
+        api.send_timed_batch(static_vertical[0])      # First vertical static (devices 0-4)
+        time.sleep(sleep)
+        
+    api.disconnect()
+    
+    
